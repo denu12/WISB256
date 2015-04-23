800d3e9d3791795d116a8c6cb8741ab11be19b66
denu12 <vanderhoog@gmail.com>
2015-04-23 15:20:13 +0200

    Part two of the assignment

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,4 +1,5 @@
 #Ask someone's name and store it in a variable 
 name = input("Name? ")
+number = input("How many greetings? ")
 #Print Hello and the variable on the correct place 
-print("Hello, %s!" % name)
+print("%s x Hello, %s!" % (number,name))
