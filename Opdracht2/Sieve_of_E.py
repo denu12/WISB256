a628f239c07a2ffa944d1467c6b5005b5d9a4c0a
denu12 <vanderhoog@gmail.com>
2015-05-05 11:57:16 +0200

    Opdracht 2 zonder bonus

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,24 +1,40 @@
 import sys
 import time
 import math
+T1 = time.time()
+
+N = int(sys.argv[1])
 
 
 
-T1 = time.perf_counter()
+
+"""
 PrimeList = []
-
-for i in range(1,int(sys.arv[1])):
+for i in range(2,N):
 	d = True
 	for p in PrimeList:
 		d = d and i % p != 0
 	if d:
-		PrimeList.add(i)
-
-		
-f = open(string(sys.arv[2]), 'w')
-for p in PrimeList:
-    f.write(p + '\n')
+		PrimeList.append(i)
+T2 = time.time()
+"""
+PrimeList = [True]*N
+for p in range(2, math.ceil(math.sqrt(N))):
+	if PrimeList[p]:
+		for i in range(p*p, N, p):
+			PrimeList[i] = False
+			
+			
+T2 = time.time()	
+number = 0
+f = open(str(sys.argv[2]), 'w')
+for p in range(2, N):
+	if PrimeList[p]:
+		number += 1
+		f.write(str(p) + '\n')
 	
 		
-T2 = time.perf_counter()
+
+print('number of primes', number)
+#print('number of primes', len(PrimeList))
 print('Time required', T2 - T1, 'sec.')
