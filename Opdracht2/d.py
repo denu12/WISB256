0104b3b877eb9170f36eacdff37f13381c6af306
denu12 <vanderhoog@gmail.com>
2015-05-18 15:10:59 +0200

    fix in d, prayer in c

--- Opdracht2/d.py
+++ Opdracht2/d.py
@@ -1,6 +1,6 @@
 
 string = input()
-codeword = input()[0]
+codeword = input()
 
 i = 0
 read = 0
@@ -10,9 +10,11 @@ result = []
 
 while(i < stop):
 	x = ord(string[i]) - 97
-	offset = ord(codeword[read]) - 97
+	offset = -(ord(codeword[read]) - 97) 
 	result.append(chr((x + offset) % 26 + 97))
 	i = i + 1
-	read = read + 1 % mod
+	read = read + 1
+	read = read % (mod - 1)
+	
 str = ""
 print(str.join(result))
